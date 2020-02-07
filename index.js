af8d5aae3b8bd00f798697bacb70d5195d80eda1
Kibaek Kim <kibaek@peoplefund.co.kr>
2020-02-07 18:50:37 +0900

    feat: 폴더삭제 함수 추가, fix: gererateImage 함수 파라미터 object로 변경 refactor: 변수 정의코드 indent 정렬

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const path = require('path')
+const fs = require('fs')
 const { app, BrowserWindow } = require('electron')
+
+// Global target directory name
+process.env.TARGET_DIR = path.join(__dirname, '/etr_tmp/');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 
+function removeDir(callback) {
+	fs.rmdir(process.env.TARGET_DIR, callback);
+}
+
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 1200,
+    width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
@@ -25,8 +33,10 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-    app.quit()
+		win = null
+		removeDir(function() {
+			app.quit()
+		})
   })
 }
 
@@ -40,7 +50,9 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    removeDir(function() {
+			app.quit()
+		})
   }
 })
 
@@ -48,7 +60,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+		createWindow()
   }
 })
 
--- webAPI.js
+++ webAPI.js
@@ -1,34 +1,39 @@
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var bodyParser = require('body-parser')
-var Jimp = require('jimp');
-var app2 = express();
-var PORT = 8082;
+const express 		= require('express');
+const fs 					= require('fs');
+const bodyParser 	= require('body-parser')
+const Jimp 				= require('jimp');
+const app2 				= express();
+const router			= express.Router();
+const PORT 				= 8082;
 
 /**
  * 이미지를 생성하는 함수
  * 색상값으로 구성 된 배열을 파라미터로 받습니다.
  * @param {*} width 
  * @param {*} height 
- * @param {*} colorList 
- * @param {*} targetDir 
+ * @param {*} bgColorList 
+ * @param {*} dirPath 
  * @param {*} extType 
  */
-var generateImages = function(width, height, colorList, targetDir, extType) {
-	colorList.map(function(bgColor, index) {
-		var image = new Jimp(width, height, bgColor, function(err, image) {
+var generateImages = function({
+	width,
+	height,
+	bgColorList,
+	extType,
+	dirPath
+}) {
+
+	bgColorList.map(function(bgColor, index) {
+		new Jimp(width, height, bgColor, function(err, image) {
 			if (err) throw err
 	
-			image.writeAsync(`${targetDir}/${bgColor}.${extType}`)
-				.then(function(data, image) {
-					console.log('file: ',image );
+			image.writeAsync(`${dirPath}/${bgColor}.${extType}`)
+				.catch(err => {
+					console.error('[Jimp ERROR] \n', err);
 				})
 		});
 	});
 }
-
-app2.use(bodyParser.json())
 
 var getImageMimeType = function(val) {
 	switch(val) {
@@ -41,33 +46,40 @@ var getImageMimeType = function(val) {
 	}
 };
 
-app2.get('/test', function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-	res.end('{"testcode":"200", "text":"Electorn Test~"}');
- });
+app2.use(bodyParser.json())
+app2.use('/api/v1', router)
+app2.use(function timeLog(req, res, next) {
+	console.log('Time: ', Date.now());
+	next();
+})
 
-app2.post('/generate', async function (req, res) {
-	var mimeType = getImageMimeType(req.body.ext_type);
-	res.setHeader('Content-Type', mimeType);
-
+router.post('/generate', async function (req, res) {
+	// var mimeType = getImageMimeType(req.body.ext_type);
 	var extType = req.body.ext_type;
 	var width = req.body.width
 	var height = req.body.is_square ? req.body.width : req.body.height
-	var bgColor = '#' + req.body.bg_color
-	var dirName = path.join(__dirname, '/public/images/')
+	var bgColor = req.body.bg_color
 	var newDirName = Date.now();
 
-	if (!fs.existsSync(`${dirName}${newDirName}`)){
-		fs.mkdirSync(`${dirName}${newDirName}`);
+	res.setHeader('Content-Type', 'application/json');
+
+	if (!fs.existsSync(`${process.env.TARGET_DIR}`)){
+		fs.mkdirSync(`${process.env.TARGET_DIR}`);
+	}
+
+	if (!fs.existsSync(`${process.env.TARGET_DIR}${newDirName}`)){
+		fs.mkdirSync(`${process.env.TARGET_DIR}${newDirName}`);
 	}
 	
-	// console.log(image);
+	var result = await generateImages({
+		width,
+		height,
+		extType,
+		bgColorList: bgColor,
+		dirPath: `${process.env.TARGET_DIR}${newDirName}`
+	});
 
-	console.log(typeof bgColor);
-	var result = await generateImages(width, height, bgColor, `${dirName}${newDirName}`, extType);
-
-	res.send(200, 'success');
-	
+	res.status(200).send('success');
 });
 
 app2.listen(PORT, function () {
