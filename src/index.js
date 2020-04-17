488d35b0be10e0fa7ac74f8918bd47a1a71b843b
Kibaek Kim <kibaek@peoplefund.co.kr>
2020-04-17 20:51:02 +0900

    fix: 불필요코드 제거

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Main from './components'
-import fs from 'fs'
-
-window.fs = fs;
 
 ReactDOM.render(<Main />, document.getElementById('app'));
